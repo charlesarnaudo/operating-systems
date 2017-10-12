40ba058fc214cc2f8c945517a47e08d36a10686f
Charles Arnaudo <charlesarnaudo@gmail.com>
2017-10-12 09:41:58 -0400

    change to scanf

--- lab-1/copy.c
+++ lab-1/copy.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
-int main() {
+
+ int main() {
+ 	char input_file[100], output_file[100];
+
 	printf("Enter path to file to copy");
-	input_file = gets(str);
+        scanf("%s", input_file);
 
 	printf("Enter path to copy file to");
-	output_file = gets(str)
+	scanf("%s", output_file);
 
 	FILE *in, *out;
 	if ((in = fopen(input_file, "r"))) {
 		if ((out != fopen(output_file, "r"))) {
+                        char ch;
 			while((ch = fgetc(in)) != EOF) {
-				fputc(ch, out)
+				fputc(ch, out);
 			}
 		}
 	} else {
