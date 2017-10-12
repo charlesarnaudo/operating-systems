c23eb989d5050e72a3aa4758bcb399d0b49b9707
Charles Arnaudo <charlesarnaudo@gmail.com>
2017-10-12 14:18:13 -0400

    ok

--- lab-1/copy.c
+++ lab-1/copy.c
@@ -2,27 +2,27 @@
 #include<stdlib.h>
 #include<unistd.h>
 
- int main() {
- 	char input_file[100], output_file[100];
+int main() {
+        char input_file[100], output_file[100];
 
-	printf("Enter path to file to copy");
+        printf("Enter path to file to copy");
         scanf("%s", input_file);
 
-	printf("Enter path to copy file to");
-	scanf("%s", output_file);
+        printf("Enter path to copy file to");
+        scanf("%s", output_file);
 
-	FILE *in, *out;
-	if ((in = fopen(input_file, "r"))) {
-		if ((out != fopen(output_file, "r"))) {
+        FILE *in, *out;
+        if ((in = fopen(input_file, "r"))) {
+                if ((out != fopen(output_file, "r"))) {
                         char ch;
-			while((ch = fgetc(in)) != EOF) {
-				fputc(ch, out);
-			}
-		}
-	} else {
-		printf("File to copy does not exist")
-		return 0;
-	}
-	printf("File copy success");
-	return 0;
+                        while ((ch = fgetc(in)) != EOF) {
+                                fputc(ch, out);
+                        }
+                }
+        } else {
+                printf("File to copy does not exist");
+                return 0;
+        }
+        printf("File copy success");
+        return 0;
 }
