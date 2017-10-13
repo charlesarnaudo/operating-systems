dd9a7d25ad5433188e66a7968bab3d8a4b964f2c
Charles Arnaudo <charlesarnaudo@gmail.com>
2017-10-13 09:36:12 -0400

    finished copy.c

--- lab-1/copy.c
+++ lab-1/copy.c
@@ -1,28 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<string.h>
 
 int main() {
-        char input_file[100], output_file[100];
+     char input_file[100], output_file[100];
+     FILE *in, *out;
 
-        printf("Enter path to file to copy");
-        scanf("%s", input_file);
+     printf("Enter path to file to copy: ");
+     scanf("%s", input_file);
+     in = fopen(input_file, "r");
 
-        printf("Enter path to copy file to");
-        scanf("%s", output_file);
+     printf("Enter path to copy file to: ");
+     scanf("%s", output_file);
+     out = fopen(output_file, "w");
 
-        FILE *in, *out;
-        if ((in = fopen(input_file, "r"))) {
-                if ((out != fopen(output_file, "r"))) {
-                        char ch;
-                        while ((ch = fgetc(in)) != EOF) {
-                                fputc(ch, out);
-                        }
-                }
-        } else {
-                printf("File to copy does not exist");
-                return 0;
-        }
-        printf("File copy success");
-        return 0;
+     if (in != NULL) {
+          if (out != NULL) {
+               char ch;
+               ch = fgetc(in);
+               while ((ch != EOF)) {
+                    fputc(ch, out);
+                    ch = fgetc(in);
+               }
+
+          } else {
+               printf("Can't create output_file\n");
+          }
+
+     } else {
+          printf("File to copy does not exist\n");
+     }
+
+     fclose(in);
+     fclose(out);
+
+     printf("Copy successful\n");
+
 }
